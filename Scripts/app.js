e7a0c8244bc5b3175e2ea79fe8923ec5d2af0502
Simon Jung <drinkwater0502@gmail.com>
2021-04-21 14:24:40 -0700

    added images through a for loop in javascript

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,15 @@
+imagelist = document.querySelectorAll('img')
+window.addEventListener('load', addimages(imagelist))
+
+
+function addimages(list) {
+    for (let i = 0 ; i < list.length; i++) {
+        imagelist[i].src = `Images/Album${i+1}.png`
+    }
+}
+
+
+
 const albums = [
     { name: "Album 1", price: 12.99, inCart: false }, 
     { name: "Album 2", price: 14.99, inCart: false }, 
